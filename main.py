14d6b92874d9b4009822c13e7e2eea6574b3c1ed
Samuel Avila <samuelmauricioavila@gmail.com>
2026-08-05 09:27:29 -0500

    Suma, resta y multiplicación terminadas

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ calc_basica=OperacionesBasicas()
 acumulativo=False
 while True:
     if acumulativo:
+        print(f"Número actual: {calc_basica.num1}")
         calc_basica.num1=calc_basica.resultado
     else:
         calc_basica.num1=float(input("Ingrese el primer número: "))
@@ -13,3 +14,31 @@ while True:
     print("5. Limpiar")
     print("6. Salir")
     opcion=input("Seleccione una opción: ")
+    if opcion=="6":
+        print ("Saliendo del programa...")
+        break
+    elif opcion=="5":
+        acumulativo=False
+        print("Se ha limpiado el acumulativo.")
+        continue
+    if opcion in ["1", "2", "3", "4"]:
+        calc_basica.num2=float(input("Ingrese el segundo número: "))
+    if opcion=="1":
+        res=calc_basica.suma()
+    elif opcion=="2":
+        res=calc_basica.resta()
+    elif opcion=="3":
+        res=calc_basica.multiplicacion()
+    elif opcion=="4":
+        res=calc_basica.division()
+    else:
+        print("Opción inválida. ")
+        continue
+    print(f"-> Resultado: {res}")
+    if isinstance(res, (int, float)):
+        acumulativo=True
+    else: 
+        acumulativo=False
+
+
+
