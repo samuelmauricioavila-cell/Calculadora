1f112ca58522062e23e5b586e49a51e76b28f52b
Bryan Paganini <est.bryan.pardo@unimilitar.edu.co>
2026-08-08 11:11:32 -0500

    operaciones cientificas terminadas

--- operaciones_cientificas.py
+++ operaciones_cientificas.py
@@ -8,14 +8,18 @@ class OperacionesCientificas:
         self.indice = 0.0
         self.angulo = 0.0
         self.resultado = 0.0
+
     def potencia(self):
         self.base = float(input("Ingrese la base:"))
         self.exponente = float(input("Ingrese el exponente:"))
+
     def raiz(self):
         self.radicando = float(input("Ingrese el radicando:"))
         self.indice = float(input("Ingrese el índice:"))
+
     def angulo(self):
         self.angulo = float(input("Ingrese el angulo en grados:"))  
+
     def potencia_enesima(self):
         self.potencia()
         if self.base == 0 and self.exponente < 0:
@@ -23,6 +27,7 @@ class OperacionesCientificas:
             return
         self.resultado = self.base ** self.exponente
         print(f"-> Resultado: {self.base} ^ {self.exponente} = {self.resultado}")
+
     def raiz_enesima(self):
         self.raiz()
         if self.indice == 0:
@@ -35,8 +40,25 @@ class OperacionesCientificas:
         else:
             self.resultado = self.radicando ** (1/self.indice)
             print(f"-> Resultado: Raiz {self.indice} de {self.radicando} = {self.resultado}")
+
     def seno(self):
         self.angulo()
         radianes = math.radians(self.angulo)
         self.resultado = math.sin(radianes)
         print(f"sen({self.angulo}°) = {round(self.resultado, 6)}")
+
+    def coseno(self):
+        self.angulo()
+        radianes = math.radians(self.angulo)
+        self.resultado = math.cos(radianes)
+        print(f"cos({self.angulo}°) = {round(self.resultado, 6)}")
+
+    def tangente(self):
+        self.angulo()
+        if (self.angulo % 100) == 90:
+            print(f"Error: La tangente de {self.angulo}° no está definida.")
+            return 
+        radianes = math.radians(self.angulo)
+        self.resultado = math.tan(radianes)
+        print(f"tan({self.angulo}°) = {round(self.resultado, 6)}")
+
